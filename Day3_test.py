c5e93fc68f7ea0fc4a18434b09c3c8213f72f587
Joshua Shawkey <joshua.shawkey@va.gov>
2020-10-07 12:58:58 -0400

    testing again

--- Day3_test.py
+++ Day3_test.py
@@ -1,9 +1,6 @@
 # convert dictionary to query string e.g. "abc=123&def=456&hij=789"
 print() # to move cursor onto new line in test window
 
-input_dict = {"abc": "123", "def": "456", "hij": "789"}
-
-
 def convert_to_query_string(dict):
     query_list = []
 
@@ -12,5 +9,6 @@ def convert_to_query_string(dict):
 
     return "&".join(query_list)
 
-print(convert_to_query_string(input_dict))
+
+print(convert_to_query_string({"abc": "123", "def": "456", "hij": "789"}))
 
